746ae25324f1088f185e92d769d449568e19fb77
wilfried <fournier.wilf@gmail.com>
2025-09-26 20:15:54 +0200

    add basic entity

---
+++ migrations/Version20250926181458.php
@@ -0,0 +1,33 @@
+<?php
+
+declare(strict_types=1);
+
+namespace DoctrineMigrations;
+
+use Doctrine\DBAL\Schema\Schema;
+use Doctrine\Migrations\AbstractMigration;
+
+/**
+ * Auto-generated Migration: Please modify to your needs!
+ */
+final class Version20250926181458 extends AbstractMigration
+{
+    public function getDescription(): string
+    {
+        return '';
+    }
+
+    public function up(Schema $schema): void
+    {
+        // this up() migration is auto-generated, please modify it to your needs
+        $this->addSql('CREATE TABLE reservation (id SERIAL NOT NULL, username VARCHAR(255) NOT NULL, starting_date TIMESTAMP(0) WITHOUT TIME ZONE NOT NULL, minute_duration INT NOT NULL, PRIMARY KEY(id))');
+        $this->addSql('COMMENT ON COLUMN reservation.starting_date IS \'(DC2Type:datetime_immutable)\'');
+    }
+
+    public function down(Schema $schema): void
+    {
+        // this down() migration is auto-generated, please modify it to your needs
+        $this->addSql('CREATE SCHEMA public');
+        $this->addSql('DROP TABLE reservation');
+    }
+}
---
+++ src/Entity/Reservation.php
@@ -0,0 +1,65 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\ReservationRepository;
+use Doctrine\ORM\Mapping as ORM;
+
+#[ORM\Entity(repositoryClass: ReservationRepository::class)]
+class Reservation
+{
+    #[ORM\Id]
+    #[ORM\GeneratedValue]
+    #[ORM\Column]
+    private ?int $id = null;
+
+    #[ORM\Column(length: 255)]
+    private ?string $username = null;
+
+    #[ORM\Column]
+    private ?\DateTimeImmutable $startingDate = null;
+
+    #[ORM\Column]
+    private ?int $minuteDuration = null;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getUsername(): ?string
+    {
+        return $this->username;
+    }
+
+    public function setUsername(string $username): static
+    {
+        $this->username = $username;
+
+        return $this;
+    }
+
+    public function getStartingDate(): ?\DateTimeImmutable
+    {
+        return $this->startingDate;
+    }
+
+    public function setStartingDate(\DateTimeImmutable $startingDate): static
+    {
+        $this->startingDate = $startingDate;
+
+        return $this;
+    }
+
+    public function getMinuteDuration(): ?int
+    {
+        return $this->minuteDuration;
+    }
+
+    public function setMinuteDuration(int $minuteDuration): static
+    {
+        $this->minuteDuration = $minuteDuration;
+
+        return $this;
+    }
+}
