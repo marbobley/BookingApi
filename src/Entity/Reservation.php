c8be3b81207aa73d8c7decea1d46675ae433e0e9
wilfried <fournier.wilf@gmail.com>
2025-09-26 20:32:26 +0200

    add ApiResourcde

--- src/Entity/Reservation.php
+++ src/Entity/Reservation.php
@@ -1,11 +1,12 @@
 <?php
 
 namespace App\Entity;
-
+use ApiPlatform\Metadata\ApiResource;
 use App\Repository\ReservationRepository;
 use Doctrine\ORM\Mapping as ORM;
 
 #[ORM\Entity(repositoryClass: ReservationRepository::class)]
+#[ApiResource]
 class Reservation
 {
     #[ORM\Id]
