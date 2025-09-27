638262128c8080ac95b1274e398c61f2784a6252
wilfried <fournier.wilf@gmail.com>
2025-09-27 12:26:49 +0200

    TDD : some basics tests

--- src/Domain/ServiceImpl/ReserverImpl.php
+++ src/Domain/ServiceImpl/ReserverImpl.php
@@ -12,8 +12,24 @@ class ReserverImpl implements ReseverInterface
     {
     }
 
+    /*
+        * @param ReservationModel $reservation
+        * @return ReservationModel|null
+    */
     public function reserver(ReservationModel $reservation): ?ReservationModel
     {
+        if (empty($reservation->getUsername())) {
+            throw new \InvalidArgumentException("Name cannot be empty");
+        }
+
+        if( $reservation->getStartingDate() < new \DateTimeImmutable("now")) {
+            throw new \InvalidArgumentException("Date cannot be in the past");
+        }
+
+        if( $reservation->getMinuteDuration() <= 0) {
+            throw new \InvalidArgumentException("Duration must be positive and different from zero");
+        }
+
         return null;
     }
 }
