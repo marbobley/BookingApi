2e03bd45af11c68e0bee3055b50f45c06e2f5870
wilfried <fournier.wilf@gmail.com>
2025-09-27 23:32:12 +0200

    add easy coding

--- src/Application/Controller/MainController.php
+++ src/Application/Controller/MainController.php
@@ -6,7 +6,6 @@ use App\Domain\Model\ReservationModel;
 use App\Domain\ServiceInterface\ReserverInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\JsonResponse;
-use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
 
 final class MainController extends AbstractController
